6eca1235223d508edced92216f3eaac96e001dce
Fabyola <fabyolacampos20@gmail.com>
2021-11-24 20:18:26 -0300

    Commit da pasta introdução javascript

---
+++ js/remover-paciente.js
@@ -0,0 +1,12 @@
+var paciente = document.querySelectorAll(".paciente");
+
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick",function(event){
+
+  event.target.parentNode.classList.add("fadeOut");
+
+ setTimeout(function(){
+   event.target.parentNode.remove();
+ },500);  
+});
